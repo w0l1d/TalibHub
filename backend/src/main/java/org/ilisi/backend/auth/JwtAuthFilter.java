b13f4a03b90ee74105a4fb81f9c67edea283b24c
abdo <abdo-pro@hotmail.com>
2023-12-19 23:31:42 +0100

    #4 Renamed variable in AuthFilter

--- backend/src/main/java/org/ilisi/backend/auth/JwtAuthFilter.java
+++ backend/src/main/java/org/ilisi/backend/auth/JwtAuthFilter.java
@@ -29,17 +29,17 @@ public class JwtAuthFilter extends OncePerRequestFilter {
         final String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
 
         String username = null;
-        String jwt = null;
+        String accessToken = null;
 
         if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-            jwt = authorizationHeader.substring(7);
-            username = jwtService.extractUsername(jwt);
+            accessToken = authorizationHeader.substring(7);
+            username = jwtService.extractUsername(accessToken);
         }
 
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = userDetailsService.loadUserByUsername(username);
 
-            if (!jwtService.validateAccessToken(jwt, userDetails)) {
+            if (!jwtService.validateAccessToken(accessToken, userDetails)) {
                 throw new RuntimeException("Access Token is not valid..!!");
             }
             UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
