597285f5595bc3417c4fe9145b96b1a2134fb8cd
musdev <w0l1d.dev@gmail.com>
2024-01-12 12:52:13 +0100

    #44 match with ignore case

--- backend/src/main/java/org/ilisi/backend/specs/ProfileSpecifications.java
+++ backend/src/main/java/org/ilisi/backend/specs/ProfileSpecifications.java
@@ -2,7 +2,11 @@ package org.ilisi.backend.specs;
 
 
 import jakarta.persistence.criteria.Join;
-import org.ilisi.backend.model.*;
+import jakarta.persistence.criteria.JoinType;
+import org.ilisi.backend.model.Education;
+import org.ilisi.backend.model.Experience;
+import org.ilisi.backend.model.Institut;
+import org.ilisi.backend.model.Profile;
 import org.springframework.data.jpa.domain.Specification;
 
 import static org.ilisi.backend.specs.SpecUtils.likePattern;
@@ -21,21 +25,31 @@ public class ProfileSpecifications {
      */
     public static Specification<Profile> profileDeeplyHasKeyword(String keyword) {
         return (root, query, builder) -> {
-            String pattern = likePattern(keyword);
-            Join<Profile, Education> educationJoin = root.join(Profile_.EDUCATIONS);
-            Join<Education, Institut> institutJoinByEducation = educationJoin.join(Education_.INSTITUT);
-            Join<Profile, Experience> experienceJoin = root.join(Profile_.EXPERIENCES);
-            Join<Experience, Institut> institutJoinByExperience = experienceJoin.join(Experience_.INSTITUT);
+            String pattern = likePattern(keyword.toLowerCase());
+            Join<Profile, Education> educationJoin = root.join(Profile_.EDUCATIONS, JoinType.LEFT);
+            Join<Education, Institut> institutJoinByEducation = educationJoin.join(Education_.INSTITUT, JoinType.LEFT);
+            Join<Profile, Experience> experienceJoin = root.join(Profile_.EXPERIENCES, JoinType.LEFT);
+            Join<Experience, Institut> institutJoinByExperience = experienceJoin.join(Experience_.INSTITUT, JoinType.LEFT);
 
             return builder.or(
-                    builder.like(educationJoin.get(Education_.TITLE), pattern),
-                    builder.like(experienceJoin.get(Education_.LOCATION), pattern),
-                    builder.like(institutJoinByEducation.get(Institut_.NAME), pattern),
-                    builder.like(experienceJoin.get(Experience_.TITLE), pattern),
-                    builder.like(experienceJoin.get(Experience_.LOCATION), pattern),
-                    builder.like(institutJoinByExperience.get(Institut_.NAME), pattern),
+                    builder.like(builder.lower(educationJoin.get(Education_.TITLE)), pattern),
+                    builder.like(builder.lower(educationJoin.get(Education_.LOCATION)), pattern),
+                    builder.like(builder.lower(educationJoin.get(Education_.STUDY_FIELD)), pattern),
+                    builder.like(builder.lower(institutJoinByEducation.get(Institut_.NAME)), pattern),
+                    builder.like(builder.lower(experienceJoin.get(Experience_.TITLE)), pattern),
+                    builder.like(builder.lower(experienceJoin.get(Experience_.LOCATION)), pattern),
+                    builder.like(builder.lower(experienceJoin.get(Experience_.DESCRIPTION)), pattern),
+                    builder.like(builder.lower(institutJoinByExperience.get(Institut_.NAME)), pattern),
                     builder.like(root.get(Profile_.ABOUT), pattern)
             );
         };
     }
+
+
+//    public static Specification<Profile> belownsTo(String studentId) {
+//        return (root, query, builder) -> {
+//            return builder.equal(root.get(Profile_.STUDENT).get(Student_.ID), studentId);
+//        };
+//    }
+
 }
