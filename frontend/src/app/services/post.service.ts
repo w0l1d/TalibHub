d8ae06a9eed86b797f9247fb5c01baae423a77dd
abdo <abdo-pro@hotmail.com>
2024-01-28 21:23:08 +0100

    #62 all static components added and running well

--- frontend/src/app/services/post.service.ts
+++ frontend/src/app/services/post.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {environment as env} from "../../environments/environment.development";
 import { HttpClient } from '@angular/common/http';
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +17,9 @@ export class PostService {
   }
 
   // get all posts
-  public getAllPosts(): any {
-    return this.http.get(`${this.baseUrl}/posts`);
+  public getAllPosts(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/postes`);
   }
+
+
 }
