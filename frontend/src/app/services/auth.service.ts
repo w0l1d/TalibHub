ade2e9c60e2b0da93fa6a9d4ea7ea296e0222f8f
YASSINEJR3 <100882813+YASSINEJR3@users.noreply.github.com>
2024-01-29 16:31:17 +0100

    #65 routes guards done & user stored globally. (#66)
    * #65 routes guards done & user stored globally.
    
    * #65 routes guards done & user stored globally.
    
    ---------
    
    Co-authored-by: Anas KBL <kabila.anas1@gmail.com>
    Co-authored-by: yassine <yassine@gmail.com>

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -4,6 +4,7 @@ import {HttpClient} from "@angular/common/http";
 import {Router} from "@angular/router";
 import {catchError, of, tap} from "rxjs";
 import {Token} from "../models/token";
+import User from "../models/user";
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,7 @@ export class AuthService {
   readonly baseUrl: string;
   private readonly JWT_TOKEN = "JWT_TOKEN";
   private readonly REFRESH_TOKEN = "REFRESH_TOKEN";
-  private loggedUser: any;
+  private loggedUser?: User | null;
 
   constructor(
     private httpClient: HttpClient,
@@ -30,7 +31,7 @@ export class AuthService {
 
           next: (tokens: Token) => {
             console.log("login got response", tokens);
-            this.doLoginUser(data.username, tokens);
+            this.doLoginUser(tokens.user!, tokens);
             resolve(true);
           },
           error: (error) => {
@@ -73,15 +74,31 @@ export class AuthService {
     return localStorage.getItem(this.JWT_TOKEN);
   }
 
-  private doLoginUser(username: string, tokens: Token) {
-    this.loggedUser = username;
-    this.storeTokens(tokens);
+  isAuthenticated(): boolean {
+    return !!this.loggedUser;
   }
 
   private doLogoutUser() {
     this.loggedUser = null;
     this.removeTokens();
     this.router.navigate(["/login"]).then(() => console.log("navigated to login"));
+  }
+
+  isStudent(): boolean {
+    return this.isAuthenticated() && this.loggedUser?.authorities?.at(0).authority === "ROLE_STUDENT";
+  }
+
+  isManager(): boolean {
+    return this.isAuthenticated() && this.loggedUser?.authorities?.at(0).authority === "ROLE_MANAGER";
+  }
+
+  getLoggedUser(): User {
+    return localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")!) : null;
+  }
+
+  private doLoginUser(user: User, tokens: Token) {
+    this.loggedUser = user;
+    this.storeTokens(tokens);
   }
 
   private getRefreshToken() {
@@ -95,11 +112,13 @@ export class AuthService {
   private storeTokens(tokens: Token) {
     localStorage.setItem(this.JWT_TOKEN, tokens.accessToken);
     localStorage.setItem(this.REFRESH_TOKEN, tokens.refreshToken);
+    localStorage.setItem("user", JSON.stringify(tokens.user));
   }
 
   private removeTokens() {
     localStorage.removeItem(this.JWT_TOKEN);
     localStorage.removeItem(this.REFRESH_TOKEN);
+    localStorage.removeItem("user");
   }
 
 }
